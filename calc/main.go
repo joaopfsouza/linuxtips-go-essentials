6b82da17f8378927dd11f2525662c335702062da
jpfs <joao.ferreiradesouza@gmail.com>
2026-07-26 18:31:37 -0300

    feat(main): ✨ Enhance main function with improved error handling and output
    - Added error handling for invalid input values.
    - Improved output formatting for operation execution.
    - Included a greeting function to enhance user interaction.

--- calc/main.go
+++ calc/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/joaopfsouza/linuxtips-go-essentials/calc/operations"
 
@@ -9,6 +12,82 @@ import (
 )
 
 func main() {
+
+	// slice (array) uma lista de valores
+	//args := os.Args
+
+	ops := os.Args[1]
+	s1 := os.Args[2]
+	s2 := os.Args[3]
+
+	n1, err := strconv.ParseFloat(s1, 64)
+	if err != nil {
+		panic("Valor invalido")
+	}
+
+	n2, err := strconv.ParseFloat(s2, 64)
+	fmt.Println(n2)
+	if err != nil {
+		panic("Valor invalido")
+	}
+
+	fmt.Printf("Executando a operação %s com valores %f e %f\n", ops, n1, n2)
+	fmt.Println(ops)
+	fmt.Println(n1)
+	fmt.Println(n2)
+
+	var result float64
+
+	switch ops {
+	case "+":
+		{
+			result = op.Sum(n1, n2)
+
+		}
+	case "-":
+		{
+			result = op.Sub(n1, n2)
+		}
+	case "*":
+		{
+			result = op.Mul(n1, n2)
+
+		}
+	case "/":
+		{
+			result, err = op.Div(n1, n2)
+
+			if err != nil {
+				fmt.Println(err)
+				panic("Valor inválido")
+			}
+
+		}
+	default:
+		{
+			fmt.Println("Operador inválido")
+			err = errors.New("Operador inválido")
+		}
+	}
+
+	if err != nil {
+		fmt.Printf("O operador %s é inválido\n", ops)
+	} else {
+
+		fmt.Printf("O resultado de %f %s %f = %f\n", n1, ops, n2, result)
+	}
+
+	sayHello("Joao", "Paulo", "Ferreira")
+
+}
+
+func sayHello(names ...string) {
+	for _, name := range names {
+		fmt.Printf("Hello %s \n", name)
+	}
+}
+
+func calc() {
 	add := operations.Sum(5, 2)
 	fmt.Println(add)
 
